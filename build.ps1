906d433debedf1ce24ae5588e5d25079e897a1b9
ctyar <ctyar@outlook.com>
2025-04-05 12:29:03 +0100

    Fix the versioning for the new project

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
 Get-ChildItem -Path '.\artifacts' | Remove-Item -Force -Recurse
 
 dotnet pack src\SwaggerUI.OpenApi\SwaggerUI.OpenApi.csproj -o artifacts
+dotnet pack src\SwaggerUI.OpenApi.Versioning\SwaggerUI.OpenApi.Versioning.csproj -o artifacts
