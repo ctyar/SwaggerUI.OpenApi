919f66c233442a13f2593e614d20eeffb62f331a
ctyar <ctyar@outlook.com>
2025-04-07 19:15:07 +0100

    Refactor single document with config test

--- test/SwaggerUI.OpenApi.Tests/ServiceCollectionExtensionsTests.cs
+++ test/SwaggerUI.OpenApi.Tests/ServiceCollectionExtensionsTests.cs
@@ -61,4 +61,62 @@ public class ServiceCollectionExtensionsTests
 
         Assert.Equal(expected, actual);
     }
+
+    [Fact]
+    public async Task AddSwaggerUIForOneDocumentWithConfig()
+    {
+        var expected = """
+            <!DOCTYPE html>
+            <html lang="en">
+              <head>
+                <meta charset="UTF-8">
+                <title>v1</title>
+                <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist/swagger-ui.css" />
+                <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist/index.css" />
+                <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist/favicon-32x32.png" sizes="32x32" />
+                <link rel="icon" type="image/png" href="https://unpkg.com/swagger-ui-dist/favicon-16x16.png" sizes="16x16" />
+              </head>
+
+              <body>
+                <div id="swagger-ui"></div>
+                <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-bundle.js" charset="UTF-8"> </script>
+                <script src="https://unpkg.com/swagger-ui-dist/swagger-ui-standalone-preset.js" charset="UTF-8"> </script>
+                <script>
+                  window.onload = function() {
+                    var configObject = JSON.parse('{"configUrl":"/configs/urls.yaml","dom_id":"#swagger-ui","urls":[{"url":"/openapi/v1.json","name":"v1"}],"layout":"StandaloneLayout","showCommonExtensions":true,"requestSnippetsEnabled":true,"supportedSubmitMethods":["get","put","post","delete","options","head","patch","trace"],"persistAuthorization":true}');
+
+                    configObject.presets = [SwaggerUIBundle.presets.apis,SwaggerUIStandalonePreset];
+                    configObject.plugins = [SwaggerUIBundle.plugins.DownloadUrl];
+
+                    if (!configObject.hasOwnProperty("oauth2RedirectUrl"))
+                      configObject.oauth2RedirectUrl = (new URL("swagger/oauth2-redirect.html", window.location.href)).href;
+
+                    const ui = SwaggerUIBundle(configObject);
+
+                    window.ui = ui;
+                  };
+                </script>
+              </body>
+            </html>
+            """;
+
+        var builder = WebApplication.CreateBuilder();
+        builder.WebHost.UseTestServer();
+        builder.Services.AddOpenApi();
+        builder.Services.AddSwaggerUI(o =>
+        {
+            o.ConfigUrl = "/configs/urls.yaml";
+        });
+
+        var app = builder.Build();
+        app.MapOpenApi();
+        app.MapSwaggerUI();
+
+        app.Start();
+        var client = app.GetTestClient();
+
+        var actual = await client.GetStringAsync("swagger", TestContext.Current.CancellationToken);
+
+        Assert.Equal(expected, actual);
+    }
 }
