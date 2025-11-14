6d6aca2bd331efc81631956ff52f384587bec5fd
ctyar <ctyar@outlook.com>
2025-11-14 20:49:07 +0000

    Update the oauth2-redirect.html to the latest version

--- src/SwaggerUI.OpenApi/Endpoints.cs
+++ src/SwaggerUI.OpenApi/Endpoints.cs
@@ -201,9 +201,10 @@ internal static class Endpoints
             <!doctype html>
             <html lang="en-US">
             <body>
+            <script src="oauth2-redirect.js"></script>
             </body>
             </html>
-            <script src="oauth2-redirect.js"></script>
+
             """;
     }
 
